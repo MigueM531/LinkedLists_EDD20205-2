db7bd358a27b3864cb6d77aa4c8242d0a235b22a
MigueM531 <MigueM531>
2025-10-03 20:57:16 -0500

    Añadir clase Vehiculo y crear instancia de DoublyLinkedList

---
+++ Autopista.py
@@ -0,0 +1,10 @@
+from clases_base import *
+
+class Vehiculo:
+    def __init__(self, placa: str, tipo: str, prioridad: int):
+        self.placa = placa
+        self.tipo = tipo
+        self.prioridad = prioridad
+
+Autopista = DoublyLinkedList()
+
