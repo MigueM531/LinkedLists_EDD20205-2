7e0a01ceaffc67f495adf0dac4056d4b173e2b1a
MigueM531 <MigueM531>
2025-10-11 22:40:12 -0500

    Implementar función de accidente para gestionar colisiones entre vehículos

--- Autopista.py
+++ Autopista.py
@@ -44,18 +44,47 @@ def ceder_paso(autopista: DoublyLinkedList):
     return
 
 
-Autopista = DoublyLinkedList()
+def accidente(autopista: DoublyLinkedList, placa_inicio, placa_final):
+    actual = autopista.head
+
+    while actual:
+        if actual.value.placa == placa_inicio:
+            accidentado1 = actual
+        if actual.value.placa == placa_final:
+            accidentado2 = actual
+        actual = actual.next
+
+    actual = accidentado1.next
+    while actual and actual != accidentado2:
+        actual.prev.next = actual.next
+        actual.next.prev = actual.prev
+        actual = actual.next
+
+    accidentado1.prev.next = accidentado1.next
+    accidentado1.next.prev = accidentado1.prev
+
+    accidentado2.prev.next = accidentado2.next
+    accidentado2.next.prev = accidentado2.prev
+
+    print(accidentado1, accidentado2)
+    return 
+    
+
+autopista = DoublyLinkedList()
 vehiculos = (
-    Vehiculo("ABC123", "auto", 3),
-    Vehiculo("ASH543", "moto", 2),
-    Vehiculo("HGF789", "moto", 1),
-    Vehiculo("EML395", "moto", 1),
-    Vehiculo("EKJ527", "camion", 5)
+    Vehiculo("ABC 123", "auto", 3),
+    Vehiculo("ASH 543", "camion", 2),
+    Vehiculo("HGF 789", "moto", 1),
+    Vehiculo("EML 395", "moto", 1),
+    Vehiculo("EKJ 527", "camion", 5),
+    Vehiculo("ENS 245", "moto", 2)
 )
 
 for v in vehiculos:
-    insertar_vehiculos(Autopista, v)
+    insertar_vehiculos(autopista, v)
 
-print(Autopista)
-ceder_paso(Autopista)
-print(Autopista)
+print(autopista)
+#ceder_paso(autopista)
+#print(autopista)
+accidente(autopista, "ASH 543", "EML 395")
+print(autopista)
